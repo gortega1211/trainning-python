960dd115659300f981234528ad160a154e471b11
Gustavo Ortega <gustavo.ortega.palacios@gmail.com>
2024-09-19 20:28:24 -0500

    [IMP] Obtencion de transacciones por fechas

--- personal_finance_tracker/main.py
+++ personal_finance_tracker/main.py
@@ -1,10 +1,12 @@
 import pandas as pd # type: ignore
 import csv
 from datetime import datetime
+from data_entry import get_date, get_amount, get_category, get_description
 
 class CSV:
     CSV_FILE = "finance_data.csv"
     COLUMNS = ["date", "amount", "category", "description"]
+    DATE_FORMAT = "%d-%m-%Y"
 
     @classmethod
     def initialize_csv(cls):
@@ -27,6 +29,41 @@ class CSV:
             writer.writerow(new_entry)
         print("Entry added successfully")
 
-if __name__ == "__main__":
+    @classmethod
+    def get_transactions(cls, start_date, end_date):
+        df = pd.read_csv(cls.CSV_FILE)
+        df["date"] = pd.to_datetime(df["date"], format=cls.DATE_FORMAT)
+        start_date = datetime.strptime(start_date, cls.DATE_FORMAT)
+        end_date = datetime.strptime(end_date, cls.DATE_FORMAT)
+
+        mask = (df["date"] >= start_date) & (df["date"] <= end_date)
+        filtered_df = df.loc[mask]
+
+        if filtered_df.empty:
+            print('No transactions found in the given date range.')
+        else:
+            print(f"Transactions from {start_date.strftime(cls.DATE_FORMAT)} to {end_date.strftime(cls.DATE_FORMAT)}")
+            print(filtered_df.to_string(index=False, formatters={"date": lambda x: x.strftime(cls.DATE_FORMAT)}))
+
+            total_income = filtered_df[filtered_df["category"] == "Income"]["amount"].sum()
+            total_expense = filtered_df[filtered_df["category"] == "Expense"]["amount"].sum()
+            print("\nSummary:")
+            print(f"{'Total Income':<18}: ${total_income:.2f}")
+            print(f"{'Total Expense':^18}: ${total_expense:.2f}")
+            print(f"{'Net Savings':>18}: ${(total_income - total_expense):.2f}")
+
+        return filtered_df
+
+def add():
     CSV.initialize_csv()
-    CSV.add_entry("19-09-2024", 125.65, "Income", "Salary")
+    date = get_date("Enter the date of the transaction (dd-mm-yyyy) or enter for today's date: ")
+    amount = get_amount()
+    category = get_category()
+    description = get_description()
+    CSV.add_entry(date, amount, category, description)
+
+if __name__ == "__main__":
+#     CSV.initialize_csv()
+#     CSV.add_entry("19-09-2024", 125.65, "Income", "Salary")
+    CSV.get_transactions("01-09-2024", "30-09-2024")
+    # add()
