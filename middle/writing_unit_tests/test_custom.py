7f8fb377d4d3addbf4753ca3468a612f54988db8
Gustavo Ortega <gustavo.ortega.palacios@gmail.com>
2024-11-09 01:39:37 -0500

    [IMP] explorando los métodos de assert disponibles

---
+++ middle/writing_unit_tests/test_custom.py
@@ -0,0 +1,17 @@
+import unittest
+
+class CustomTestCase(unittest.TestCase):
+    def assertAllIntegers(self, values):
+        for value in values:
+            self.assertIsInstance(
+                value,
+                int,
+            )
+
+class TestIntegerList(CustomTestCase):
+    def test_values_are_integers(self):
+        integers_list = [1, 2, 3, 4, 5]
+        self.assertAllIntegers(integers_list)
+
+if __name__ == "__main__":
+    unittest.main()
